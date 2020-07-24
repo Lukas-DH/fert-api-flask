168b3b16e46ac637c0ba56e50e7702e70de65921
Lukas Stojanov <git@lukas-stojanov.com>
2020-07-24 17:37:42 +0200

    import pandas and sklearn added to code

--- modgen.py
+++ modgen.py
@@ -1,4 +1,7 @@
 import pickle
+import pandas as pd
+import sklearn
+
 X_train,X_test,y_train,y_test= pd.read_pickle("X_train.pkl"),pd.read_pickle("X_test.pkl"),pd.read_pickle("y_train.pkl"),pd.read_pickle("y_test.pkl")
 from sklearn.ensemble import RandomForestClassifier
 RFC1=RandomForestClassifier(class_weight='balanced_subsample',max_depth=5,criterion='entropy',n_estimators=100)
